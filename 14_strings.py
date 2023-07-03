170da47f51bebce7e6f61f8f279f1bc0582345eb
Alejo <alejandrovanegas09@outlook.com>
2023-07-03 15:01:49 +0000

    String method

--- 14_strings.py
+++ 14_strings.py
@@ -1,2 +1,14 @@
 text='Ella sabe programar en Python'
-print()
+print(text)
+
+print(text.isupper())
+print(text.lower())
+print(text.swapcase())
+print(text.replace("Python","Go"))
+
+text_2='este es un titulo'
+print(text_2.capitalize())
+print(text_2.title())
+
+print("153".isdigit())
+print("153.4".isdigit())
